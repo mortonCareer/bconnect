2b1b03b29a37db546f1f224bfd573c5a18f64d2a
fine-pine <lsm3645@g.skku.edu>
2026-01-04 23:02:25 +0900

    feat: Post, Task, User 도메인 클래스 생성

---
+++ apps/api/core/src/main/java/so/morton/api/domain/Task.java
@@ -0,0 +1,43 @@
+package so.morton.api.domain;
+
+import so.morton.api.storage.entity.Address;
+import so.morton.api.storage.entity.TaskEntity;
+import so.morton.api.storage.value.Trade;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public record Task(
+    Long id,
+    // TODO: 삭제
+    String company,
+    Address address,
+    // TODO: 추가
+    // projectId: Long [nullable]
+    // crewId: Long [nullable]
+    String taskTitle,
+    String eventTitle,
+    Set<Trade> trades,
+    LocalDate start,
+    LocalDate end,
+    // TODO: 추가
+    // status: TaskStatus
+    LocalDateTime createdAt,
+    LocalDateTime modifiedAt
+) {
+    public static Task of(TaskEntity entity) {
+        return new Task(
+                entity.getId(),
+                entity.getCompany(),
+                entity.getAddress(),
+                entity.getTaskTitle(),
+                entity.getEventTitle(),
+                entity.getTrades(),
+                entity.getStart(),
+                entity.getEnd(),
+                entity.getCreatedAt(),
+                entity.getModifiedAt()
+        );
+    }
+}
