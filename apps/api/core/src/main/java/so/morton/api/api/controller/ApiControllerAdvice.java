eeba2736a3c639e7f35842ad9fc1a9a6a36b23ef
fine-pine <lsm3645@g.skku.edu>
2026-01-04 23:05:01 +0900

    chore: Controller 공통 응답 및 에러 처리

---
+++ apps/api/core/src/main/java/so/morton/api/api/controller/ApiControllerAdvice.java
@@ -0,0 +1,38 @@
+package so.morton.api.api.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import so.morton.api.support.CodeException;
+import so.morton.api.support.CommonExceptionCode;
+import so.morton.api.support.response.ApiResponse;
+
+@RestControllerAdvice
+public class ApiControllerAdvice {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @ExceptionHandler(CodeException.class)
+    public ResponseEntity<ApiResponse<Void>> handleCodeException(CodeException e) {
+        if (e.getExceptionCode().getStatus().is5xxServerError()) {
+            log.error("CodeException : {}", e.getMessage(), e);
+        } else {
+            log.warn("CodeException : {}", e.getMessage(), e);
+        }
+
+        return ResponseEntity
+                .status(e.getExceptionCode().getStatus())
+                .body(ApiResponse.error(e.getExceptionCode()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
+        log.error("Exception : {}", e.getMessage(), e);
+
+        return ResponseEntity
+                .status(CommonExceptionCode.INTERNAL_SERVER_ERROR.getStatus())
+                .body(ApiResponse.error(CommonExceptionCode.INTERNAL_SERVER_ERROR));
+    }
+}
