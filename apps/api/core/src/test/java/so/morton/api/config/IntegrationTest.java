118f875e4a359a4f7f003ee8f047071748693bec
fine-pine <lsm3645@g.skku.edu>
2026-03-27 01:34:56 +0900

    test: add test config annotation

---
+++ apps/api/core/src/test/java/so/morton/api/config/IntegrationTest.java
@@ -0,0 +1,18 @@
+package so.morton.api.config;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+public @interface IntegrationTest {
+}
