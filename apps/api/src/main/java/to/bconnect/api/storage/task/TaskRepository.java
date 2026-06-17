ebbe72eb2e14bffe459ef2656dd455add317b865
fine-pine <lsm3645@g.skku.edu>
2026-06-17 13:38:54 +0900

    feat(be): update task entity to sprint 3

--- apps/api/src/main/java/to/bconnect/api/storage/task/TaskRepository.java
+++ apps/api/src/main/java/to/bconnect/api/storage/task/TaskRepository.java
@@ -5,5 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
-    List<TaskEntity> findAllByMemberId(Long memberId);
+    List<TaskEntity> findAllByWorkerId(Long workerId);
+
+    List<TaskEntity> findAllByWorkerIdAndType(Long workerId, TaskType type);
+
+    List<TaskEntity> findAllByProjectId(Long projectId);
 }
