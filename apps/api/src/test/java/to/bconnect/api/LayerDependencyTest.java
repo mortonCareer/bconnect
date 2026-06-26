598c59cb6a368fb6d6b05774106d27f1bb460247
fine-pine <lsm3645@g.skku.edu>
2026-06-26 21:54:16 +0900

    test(api): ArchUnit 테스트 임시 중단 (#664)

--- apps/api/src/test/java/to/bconnect/api/LayerDependencyTest.java
+++ apps/api/src/test/java/to/bconnect/api/LayerDependencyTest.java
@@ -1,16 +1,16 @@
 package to.bconnect.api;
 
 
-import com.tngtech.archunit.base.DescribedPredicate;
-import com.tngtech.archunit.core.domain.JavaClass.Predicates;
 import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
+import org.junit.jupiter.api.Disabled;
 
 import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
 
+@Disabled("temp (#664)")
 @AnalyzeClasses(packages = {"to.bconnect.api"}, importOptions = DoNotIncludeTests.class)
 public class LayerDependencyTest {
 
--- apps/api/src/test/java/to/bconnect/api/PackageDependencyTest.java
+++ apps/api/src/test/java/to/bconnect/api/PackageDependencyTest.java
@@ -6,11 +6,13 @@ import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
+import org.junit.jupiter.api.Disabled;
 
 import static com.tngtech.archunit.core.domain.JavaClass.Predicates.resideInAnyPackage;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
 
+@Disabled("temp (#664)")
 @AnalyzeClasses(packages = "to.bconnect.api", importOptions = DoNotIncludeTests.class)
 public class PackageDependencyTest {
 
