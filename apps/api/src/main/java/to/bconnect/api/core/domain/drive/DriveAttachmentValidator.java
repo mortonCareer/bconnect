cdc92195604299883b35a4f1d2f6a7073c72d22d
fine-pine <lsm3645@g.skku.edu>
2026-07-03 02:42:59 +0900

    feat(api): Drive Domain 계층 구현

---
+++ apps/api/src/main/java/to/bconnect/api/core/domain/drive/DriveAttachmentValidator.java
@@ -0,0 +1,23 @@
+package to.bconnect.api.core.domain.drive;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import to.bconnect.api.attachment.AttachmentContextValidator;
+import to.bconnect.api.storage.attachment.AttachmentContext;
+
+@Component
+@RequiredArgsConstructor
+public class DriveAttachmentValidator implements AttachmentContextValidator {
+
+    private final DriveValidator driveValidator;
+
+    @Override
+    public AttachmentContext context() {
+        return AttachmentContext.DRIVE;
+    }
+
+    @Override
+    public void validate(Long memberId, Long contextId) {
+        driveValidator.validate(contextId, memberId);
+    }
+}
