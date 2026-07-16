ddaec631f19eeab6375f3bd5dc2300b125424d49
fine-pine <lsm3645@g.skku.edu>
2026-07-16 14:29:48 +0900

    feat(api): 탈퇴 회원 응답 분기처리

--- apps/api/src/main/java/to/bconnect/api/core/presentation/v1/response/DirectChatResponse.java
+++ apps/api/src/main/java/to/bconnect/api/core/presentation/v1/response/DirectChatResponse.java
@@ -1,22 +1,23 @@
 package to.bconnect.api.core.presentation.v1.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import to.bconnect.api.core.domain.chat.DirectChat;
 import to.bconnect.api.core.domain.member.Member;
 
 import java.time.LocalDateTime;
 
 public record DirectChatResponse(
-        Long id,
-        MemberSummaryResponse member,
-        MessageResponse lastMessage,
-        Long unreadCount,
-        LocalDateTime createdAt,
-        LocalDateTime modifiedAt
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED) Long id,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED) WithdrawableMemberResponse member,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, nullable = true) MessageResponse lastMessage,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED) Long unreadCount,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED) LocalDateTime createdAt,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED) LocalDateTime modifiedAt
 ) {
     public static DirectChatResponse of(DirectChat directChat, Member member, String picture) {
         return new DirectChatResponse(
                 directChat.id(),
-                MemberSummaryResponse.of(member, picture),
+                WithdrawableMemberResponse.of(member, picture),
                 directChat.lastMessage() != null ? MessageResponse.of(directChat.lastMessage()) : null,
                 directChat.unreadCount(),
                 directChat.createdAt(),
--- apps/api/src/main/java/to/bconnect/api/core/presentation/v1/response/GroupChatResponse.java
+++ apps/api/src/main/java/to/bconnect/api/core/presentation/v1/response/GroupChatResponse.java
@@ -1,5 +1,6 @@
 package to.bconnect.api.core.presentation.v1.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import to.bconnect.api.core.domain.chat.GroupChat;
 import to.bconnect.api.core.domain.member.Member;
 
@@ -8,20 +9,20 @@ import java.util.List;
 import java.util.Map;
 
 public record GroupChatResponse(
-        Long id,
-        String title,
-        List<MemberSummaryResponse> participants,
-        MessageResponse lastMessage,
-        Long unreadCount,
-        LocalDateTime createdAt,
-        LocalDateTime modifiedAt
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED) Long id,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED) String title,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED) List<WithdrawableMemberResponse> participants,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, nullable = true) MessageResponse lastMessage,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED) Long unreadCount,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED) LocalDateTime createdAt,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED) LocalDateTime modifiedAt
 ) {
     public static GroupChatResponse of(GroupChat chat, List<Member> members, Map<Long, String> pictureMap) {
         return new GroupChatResponse(
                 chat.id(),
                 chat.title(),
                 members.stream()
-                        .map(it -> MemberSummaryResponse.of(it, pictureMap.get(it.id())))
+                        .map(it -> WithdrawableMemberResponse.of(it, pictureMap.get(it.id())))
                         .toList(),
                 chat.lastMessage() != null ? MessageResponse.of(chat.lastMessage()) : null,
                 chat.unreadCount(),
