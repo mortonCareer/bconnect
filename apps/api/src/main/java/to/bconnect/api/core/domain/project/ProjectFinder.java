e6c81ba9aac13df5c3a37c33b53f9b08913ba90d
fine-pine <lsm3645@g.skku.edu>
2026-07-25 21:29:45 +0900

    feat(api): 작업 응답에 프로젝트 업체 정보 추가 (#1044)

--- apps/api/src/main/java/to/bconnect/api/core/domain/project/ProjectFinder.java
+++ apps/api/src/main/java/to/bconnect/api/core/domain/project/ProjectFinder.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import to.bconnect.api.common.CodeException;
 import to.bconnect.api.common.CommonExceptionCode;
+import to.bconnect.api.core.domain.company.Company;
 import to.bconnect.api.storage.Address;
+import to.bconnect.api.storage.company.CompanyEntity;
 import to.bconnect.api.storage.company.CompanyRepository;
 import to.bconnect.api.storage.project.ProjectEntity;
 import to.bconnect.api.storage.project.ProjectRepository;
@@ -35,6 +37,18 @@ public class ProjectFinder {
     public Map<Long, Address> addressMap(Collection<Long> projectIds) {
         return projectRepository.findAllById(projectIds).stream()
                 .collect(Collectors.toMap(ProjectEntity::getId, ProjectEntity::getAddress));
+    }
+
+    @Transactional(readOnly = true)
+    public Map<Long, Company> companyMap(Collection<Long> projectIds) {
+        val projects = projectRepository.findAllById(projectIds);
+        val companyIds = projects.stream().map(ProjectEntity::getCompanyId).distinct().toList();
+        val companyById = companyRepository.findAllById(companyIds).stream()
+                .collect(Collectors.toMap(CompanyEntity::getId, Company::of));
+
+        return projects.stream()
+                .filter(it -> companyById.containsKey(it.getCompanyId()))
+                .collect(Collectors.toMap(ProjectEntity::getId, it -> companyById.get(it.getCompanyId())));
     }
 
     @Transactional(readOnly = true)
