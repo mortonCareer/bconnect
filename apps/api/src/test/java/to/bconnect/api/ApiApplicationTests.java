91fff4c74365d2bb704f9a813a0ed050e240c08e
fine-pine <lsm3645@g.skku.edu>
2026-07-26 20:42:40 +0900

    chore(api): jacoco 도입 및 테스트 기본 프로필 변경

--- apps/api/src/test/java/to/bconnect/api/ApiApplicationTests.java
+++ apps/api/src/test/java/to/bconnect/api/ApiApplicationTests.java
@@ -3,9 +3,7 @@ package to.bconnect.api;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
 @SpringBootTest
 class ApiApplicationTests {
 
