bb792e8f247689aed2db17b6ca055d4cd654e20f
fine-pine <lsm3645@g.skku.edu>
2026-08-02 10:47:59 +0900

    chore(api): 테스트 데이터 컨벤션 정합

--- apps/api/src/main/resources/data-crawler.sql
+++ apps/api/src/main/resources/data-crawler.sql
@@ -3,18 +3,18 @@
 -- 테스트 (100 ~ 199)
 -- 기술자 크롤링
 INSERT INTO crawled_members (id, company, name, phone, picture, role, brn, email, created_at, modified_at) VALUES
-(100, '업체1', '기술자1', '01000000012', NULL, '반장', '123-45-67890', 'crawled1@test.com', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
-(101, '업체2', '기술자2', '01000000013', NULL, '반장', '123-45-67891', 'crawled2@test.com', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
-(102, '업체3', '기술자3', '01000000014', NULL, '팀장', '123-45-67892', 'crawled3@test.com', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
-(103, '업체4', '기술자4', '01000000015', NULL, '반장', '123-45-67893', 'crawled4@test.com', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
-(104, '업체5', '기술자5', '01000000016', NULL, '대표', '123-45-67894', 'crawled5@test.com', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00');
+(100, '업체1', '기술자1', '01000000012', NULL, '반장', '000-00-00001', 'crawled1@test.com', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
+(101, '업체2', '기술자2', '01000000013', NULL, '반장', '000-00-00002', 'crawled2@test.com', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
+(102, '업체3', '기술자3', '01000000014', NULL, '팀장', '000-00-00003', 'crawled3@test.com', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
+(103, '업체4', '기술자4', '01000000015', NULL, '반장', '000-00-00004', 'crawled4@test.com', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
+(104, '업체5', '기술자5', '01000000016', NULL, '대표', '000-00-00005', 'crawled5@test.com', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00');
 
 INSERT INTO crawled_profiles (id, member_id, primary_trade, experience, headline, address, state, url, platform, created_at, modified_at) VALUES
-(100, 100, '방수', 10, '한줄소개', '주소', '서울', 'https://blog.naver.com/crawled1', 'NAVER', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
-(101, 101, '타일', 8, '한줄소개', '주소', '경기', 'https://blog.naver.com/crawled2', 'NAVER', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
-(102, 102, '도장', 12, '한줄소개', '주소', '인천', 'https://www.instagram.com/crawled3', 'INSTAGRAM', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
-(103, 103, '전기', 15, '한줄소개', '주소', '부산', 'https://blog.naver.com/crawled4', 'NAVER', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
-(104, 104, '목공', 20, '한줄소개', '주소', '대구', 'https://www.instagram.com/crawled5', 'INSTAGRAM', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00');
+(100, 100, '방수', 10, '한줄소개', '경기 수원시 장안구 서부로 2066', '경기', 'https://blog.naver.com/crawled1', 'NAVER', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
+(101, 101, '타일', 8, '한줄소개', '경기 수원시 장안구 서부로 2066', '경기', 'https://blog.naver.com/crawled2', 'NAVER', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
+(102, 102, '도장', 12, '한줄소개', '경기 수원시 장안구 서부로 2066', '경기', 'https://www.instagram.com/crawled3', 'INSTAGRAM', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
+(103, 103, '전기', 15, '한줄소개', '경기 수원시 장안구 서부로 2066', '경기', 'https://blog.naver.com/crawled4', 'NAVER', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
+(104, 104, '목공', 20, '한줄소개', '경기 수원시 장안구 서부로 2066', '경기', 'https://www.instagram.com/crawled5', 'INSTAGRAM', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00');
 
 INSERT INTO crawled_profile_trades (profile_id, trade) VALUES
 (100, '방수'),
@@ -24,11 +24,11 @@ INSERT INTO crawled_profile_trades (profile_id, trade) VALUES
 (104, '목공');
 
 INSERT INTO crawled_tasks (id, company, address, trade, start_date, end_date, duration, created_at, modified_at) VALUES
-(100, '업체1', '주소', '방수', DATE '2026-06-01', DATE '2026-06-03', '3일', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
-(101, '업체2', '주소', '타일', DATE '2026-06-04', DATE '2026-06-06', '3일', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
-(102, '업체3', '주소', '도장', DATE '2026-06-07', DATE '2026-06-11', '5일', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
-(103, '업체4', '주소', '전기', DATE '2026-06-12', DATE '2026-06-13', '2일', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
-(104, '업체5', '주소', '목공', DATE '2026-06-14', DATE '2026-06-20', '7일', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00');
+(100, '업체1', '경기 수원시 장안구 서부로 2066', '방수', DATE '2026-06-01', DATE '2026-06-03', '3일', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
+(101, '업체2', '경기 수원시 장안구 서부로 2066', '타일', DATE '2026-06-04', DATE '2026-06-06', '3일', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
+(102, '업체3', '경기 수원시 장안구 서부로 2066', '도장', DATE '2026-06-07', DATE '2026-06-11', '5일', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
+(103, '업체4', '경기 수원시 장안구 서부로 2066', '전기', DATE '2026-06-12', DATE '2026-06-13', '2일', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
+(104, '업체5', '경기 수원시 장안구 서부로 2066', '목공', DATE '2026-06-14', DATE '2026-06-20', '7일', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00');
 
 INSERT INTO crawled_posts (id, member_id, task_id, title, content, created_at, modified_at) VALUES
 (100, 100, 100, '제목', '내용', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
@@ -54,29 +54,29 @@ INSERT INTO crawled_credentials (id, member_id, type, name, created_at, modified
 -- 샘플 (200 ~ 299)
 -- 원클릭 크롤링
 INSERT INTO kiscon_registration (ncr_gs_seq, biz_reg_no, company_name, representative, trade_name, trade_reg_no, address, region, region_detail, reg_date, announce_date, flag, phone, announce_number, announce_reason, synced_at) VALUES
-(200, '2001234567', '샘플건설', '김샘플', '실내건축공사업', '서울-실내-200', '서울 강남구 테헤란로 100', '서울', '강남구', 20200101, 20200110, '신규', '025550200', '서울 강남구공고-제2020-1호', '-', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
-(201, '2001234567', '샘플건설', '김샘플', '철근콘크리트공사업', '서울-철콘-201', '서울 강남구 테헤란로 100', '서울', '강남구', 20210301, 20210310, '신규', '025550200', '서울 강남구공고-제2021-2호', '-', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00');
+(200, '0000000001', '정상 업체1', '대표자', '실내건축공사업', '경기-실내-00001', '경기 수원시 장안구 서부로 2066', '경기', '수원시 장안구', 20260101, 20260101, '신규', '000000001', '공고번호', '-', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00'),
+(201, '0000000001', '정상 업체1', '대표자', '철근콘크리트공사업', '경기-철콘-00002', '경기 수원시 장안구 서부로 2066', '경기', '수원시 장안구', 20260101, 20260101, '신규', '000000001', '공고번호', '-', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00');
 
 INSERT INTO kiscon_admin_penalty (ncr_gs_seq, biz_reg_no, company_name, representative, trade_name, trade_reg_no, address, region, region_detail, penalty_type, violation_content, violation_detail, penalty_ground, fine_amount, penalty_amount, stop_start_date, stop_end_date, cancel_date, correction, penalty_date, announce_date, flag, phone, has_injunction, announce_number, announce_reason, synced_at) VALUES
-(200, '2001234567', '샘플건설', '김샘플', '실내건축공사업', '서울-실내-200', '서울 강남구 테헤란로 100', '서울', '강남구', '영업정지', '건설업 등록기준 미달', '자본금 미달', '건설산업기본법 제83조', 0, 0, '20250101', '20250331', NULL, NULL, 20241220, 20241228, '신규', '025550200', 'N', '서울 공고-제2024-9호', '-', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00');
+(200, '0000000001', '정상 업체1', '대표자', '실내건축공사업', '경기-실내-00001', '경기 수원시 장안구 서부로 2066', '경기', '수원시 장안구', '영업정지', '건설업 등록기준 미달', '자본금 미달', '건설산업기본법 제83조', 0, 0, '20260101', '20261231', NULL, NULL, 20260101, 20260101, '신규', '000000001', 'N', '공고번호', '-', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00');
 
 INSERT INTO kiscon_arrears (id, seq_no, company_name, address, representative, representative_age, representative_address, penalty_history, penalty_dates, arrears_amount, publication_period, synced_at) VALUES
-(200, '1', '체불건설', '서울 서초구 서초대로 200', '박체불', '52', '서울 서초구', '시정명령', '(25.12.15)', '120,000', '2026-01-01 ~ 2026-12-31', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00');
+(200, '1', '체불 업체1', '경기 수원시 장안구 서부로 2066', '대표자', '52', '경기 수원시 장안구', '시정명령', '(26.01.01)', '120,000', '2026-01-01 ~ 2026-12-31', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00');
 
 INSERT INTO kiscon_subcon_limits (id, seq_no, violation_type, company_name, corp_no, biz_reg_no, representative, restriction_start, restriction_end, category, announcement_date, certificate_url, note, synced_at) VALUES
-(200, '1', '건설산업기본법 제82조', '체불건설', '1101110000200', '2009876543', '박체불', '20260101', '20261231', '건축', '20251215', NULL, NULL, TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00');
+(200, '1', '건설산업기본법 제82조', '체불 업체1', '0000000000001', '0000000002', '대표자', '20260101', '20261231', '건축', '20260101', NULL, NULL, TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00');
 
 INSERT INTO moel_wage_defaults (id, period, name, age, company_name, industry, personal_address, company_address, arrears_amount, synced_at) VALUES
-(200, '2026.01 ~ 2026.12', '박체불', '52', '체불건설', '건설업', '서울 서초구', '서울 서초구 서초대로 200', '85,000,000', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00');
+(200, '2026.01 ~ 2026.12', '대표자', '52', '체불 업체1', '건설업', '경기 수원시 장안구', '경기 수원시 장안구 서부로 2066', '85,000,000', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00');
 
 INSERT INTO ecic_electrical_licenses (id, registration_no, company_name, representative, address, synced_at) VALUES
-(200, '서울-2026-200', '샘플건설', '김샘플', '서울 강남구 테헤란로 100', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00');
+(200, '경기-2026-00001', '정상 업체1', '대표자', '경기 수원시 장안구 서부로 2066', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00');
 
 INSERT INTO feia_fire_licenses (id, seq_no, company_name, ceo_name, address, business_type, license_div, postal_code, phone, region, region_detail, synced_at) VALUES
-(200, 1, '샘플건설', '김샘플', '서울 강남구 테헤란로 100', '공사업', '전문', '06234', '02-555-0200', '서울', '강남구', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00');
+(200, 1, '정상 업체1', '대표자', '경기 수원시 장안구 서부로 2066', '공사업', '전문', '16419', '00-000-0001', '경기', '수원시 장안구', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00');
 
 INSERT INTO cwma_retirement_fund (id, seq_no, project_name, total_amount, start_date, end_date, company_name, normalized_company_name, client_org, address, synced_at) VALUES
-(200, 1, '샘플 프로젝트 신축공사', 15, DATE '2026-01-01', DATE '2026-12-31', '샘플건설', '샘플건설', '서울특별시', '서울 강남구 테헤란로 100', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00');
+(200, 1, '프로젝트', 15, DATE '2026-01-01', DATE '2026-12-31', '정상 업체1', '정상 업체1', '기관명', '경기 수원시 장안구 서부로 2066', TIMESTAMP WITH TIME ZONE '2026-01-01 00:00:00+00');
 
 -- 실데이터 (1000 ~)
 ALTER TABLE crawled_members ALTER COLUMN id RESTART WITH 1000;
